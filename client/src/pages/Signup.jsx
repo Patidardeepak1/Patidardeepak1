532c1bb2baefbfdf5d809fd03631309aff10ac37
Deepak Patidar <deepak2025patidar@gmail.com>
2024-01-17 15:33:48 +0530

    complete sign up UI

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -11,6 +11,8 @@ function Signup() {
       <input type="text" placeholder='password' className='border p-3 rounded-l' />
       <button className="bg-slate-700 text-white p-3 rounded-lg  uppercase  hover:opacity-95 disabled:opacity-80 ">Sign Up</button>
      </form>
+
+     
      <div className='flex gap-2 mt-5'>
       <p>Have an account?</p>
       <Link to={"/sign-in"}>
