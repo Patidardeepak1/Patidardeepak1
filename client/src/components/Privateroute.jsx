0dff759ec2a1f0710d145f87cd1fcab6033378c6
Deepak Patidar <deepak2025patidar@gmail.com>
2024-01-25 18:21:26 +0530

    complete profile page ui

--- client/src/components/Privateroute.jsx
+++ client/src/components/Privateroute.jsx
@@ -5,8 +5,8 @@ import { Navigate, Outlet } from "react-router-dom"
 
 
 function Privateroute() {
-const CurrentUser=useSelector(state=>state.user)
-  return CurrentUser ? <Outlet/> : <Navigate to='/sign-in'/>
+const CurrentUser=useSelector((state) => state.user)
+  return CurrentUser ? <Outlet /> : <Navigate to='/sign-in'/>
 }
 
 export default Privateroute
